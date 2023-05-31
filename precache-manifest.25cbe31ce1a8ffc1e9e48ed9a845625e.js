24afef56b0298755b1111dfe8460989fa127d895
Utkersh uppal <utkersh2003uppal@gmail.com>
2023-05-31 20:42:55 +0530

    Updates

---
+++ precache-manifest.25cbe31ce1a8ffc1e9e48ed9a845625e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e32b8b76d570a1505688",
+    "url": "/_TextUtills_/static/css/main.f441c591.chunk.css"
+  },
+  {
+    "revision": "e32b8b76d570a1505688",
+    "url": "/_TextUtills_/static/js/main.d2fb0b33.chunk.js"
+  },
+  {
+    "revision": "91ed776631e3402a87a8",
+    "url": "/_TextUtills_/static/js/runtime~main.26d5f228.js"
+  },
+  {
+    "revision": "bf5e42d3724f4e3f779b",
+    "url": "/_TextUtills_/static/js/2.e6cead8f.chunk.js"
+  },
+  {
+    "revision": "bd2db5e315108ebec595",
+    "url": "/_TextUtills_/static/js/3.5b0ae9b5.chunk.js"
+  },
+  {
+    "revision": "05e868d72d99e1e65b25ea3d65292097",
+    "url": "/_TextUtills_/static/media/Light.05e868d7.jpg"
+  },
+  {
+    "revision": "b94621ec87c6eb9f6244c06c19ec111a",
+    "url": "/_TextUtills_/static/media/Dark_3.b94621ec.jpg"
+  },
+  {
+    "revision": "2a100a4ac80267db57c0693115bf460f",
+    "url": "/_TextUtills_/index.html"
+  }
+];
